5c0eba38e7d9371cafdf3299431a80c6bc763d3e
iWallLab <134191336+iWallLab@users.noreply.github.com>
2024-07-15 20:55:03 -0300

    Criando novas ações no InputMap
    Adicionado suporte a gamepad e keyboard

--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,7 @@ var is_running: bool = false #checkbox do space, se era sim vira nao, se nao vir
 
 #troca animacao do player
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"): 
+	if Input.is_action_just_pressed("move_up"): 
 		if is_running:
 			animation_player.play("idle")
 			is_running = false
