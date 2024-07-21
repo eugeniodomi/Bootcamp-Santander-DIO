c2367d57dfb3dda965a54de378ca835648cf7f45
iWallLab <134191336+iWallLab@users.noreply.github.com>
2024-07-21 17:40:40 -0300

    Criando o script Enemy
    Correção de bugs e modificação do código para funcionar após criar classe enemy, adaptando a func follow player.

--- enemies/behaviors (comportamentos)/follow_player.gd
+++ enemies/behaviors (comportamentos)/follow_player.gd
@@ -1,22 +1,31 @@
-extends CharacterBody2D
+extends Node
 
 @export var speed: float = 1
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D #variavel carregada apenas quando este node estiver completo
+#@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D #variavel carregada apenas quando este node estiver completo
+
+var enemy: Enemy #var criada para puxar comportamentos do parent node2d
+var sprite: AnimatedSprite2D
 
 
+func _ready():
+	enemy = get_parent()
+	sprite = enemy.get_node("AnimatedSprite2D")
+
+
+#comportamento do objeto: seguir o personagem player
 
 func _physics_process(delta: float) -> void:
 	#var player_position = Vector2(0, 0) / depreceated
 	
 	# Calcula a direcao 
 	var player_position = GameManager.player_position
-	var difference = player_position - position 
+	var difference = player_position - enemy.position 
 	var input_vector = difference.normalized() #diferenca normalizada
 	
 	# Movimento
-	velocity = input_vector * speed * 100.0
-	move_and_slide()
+	enemy.velocity = input_vector * speed * 100.0
+	enemy.move_and_slide()
 
 
 #script com calculos de vetores teste
