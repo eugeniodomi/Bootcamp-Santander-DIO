a6f5f59437b043a1811a0850b2bdbd89e923e6e4
iWallLab <134191336+iWallLab@users.noreply.github.com>
2024-07-27 22:37:06 -0300

    feature: drops items
    Adicionado drops de items de inimigos, com sistema de roleta implementado para chance de cair os itens.

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,11 +1,17 @@
 class_name Enemy
 extends Node2D
 
+@export_category("Life")
 @export var health: int = 10
 @export var death_prefab: PackedScene
 var damage_digit_prefab: PackedScene
 
 @onready var damage_digit_marker = $DamageDigitMarker
+
+@export_category("Drops")
+@export var drop_chance: float = 0.1
+@export var drop_items: Array[PackedScene]
+@export var drop_chances: Array[float]
 
 func _ready():
 	damage_digit_prefab = preload("res://misc/damage_digit.tscn") #damage digit, acessando cena
@@ -44,4 +50,43 @@ func die() -> void:
 		get_parent().add_child(death_object)
 	
 	queue_free()
+	
+	#Drop
+	if randf() <= drop_chance:
+		drop_item()
+	
+	# Deletar node
+	queue_free()
 
+func drop_item() -> void:
+	var drop = get_random_drop_item().instantiate()
+	drop.position = position
+	get_parent().add_child(drop)
+
+func get_random_drop_item() -> PackedScene:
+	# Listas com 1 item
+	if drop_items.size() == 1:
+		return drop_items[0]
+
+
+	# Calcular chance maxima
+	var max_chance: float = 0.0
+	for drop_chance in drop_chances:
+		max_chance += drop_chance
+	
+	# Jogar dado
+	var random_value = randf() * max_chance
+	
+	
+	# Girar a roleta
+	var needle: float = 0.0
+	for i in drop_items.size():
+		var drop_item = drop_items[i]
+		var drop_chance = drop_chances[i] if i < drop_chances.size() else 1
+		if random_value <= drop_chance + needle:
+			return drop_item
+		needle += drop_chance
+	
+	return drop_items[0]
+	
+	pass
